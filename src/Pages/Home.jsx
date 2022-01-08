67f114daf592ebb39824f3de587b8a9d9e01401c
vishalrranjan <vishalkr275@gmail.com>
2022-01-09 00:00:23 +0530

    added yaml file and created home component

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <input type="text" placeholder="Enter Name" />
+      <br />
+      <div>
+        <p>text will be print here</p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
